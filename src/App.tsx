b4eb008b2cd064d5861cc8af8f2c48f643cf751a
rafael-p-s <rafaelsky321@gmail.com>
2023-08-07 18:42:37 -0300

    Finished First Screen.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,17 @@ import PrincipalName from "./components/PrincipalName.tsx";
 import ItemsKnowloge from "./components/ItemsKnowlege.tsx";
 import FacebookButton from "./components/FacebookButton.tsx";
 import Icon from "./components/LinkeDinButton.tsx";
-import { Facebook, Github, Linkedin, Stethoscope, Plane } from "lucide-react";
+import {
+  Facebook,
+  Github,
+  Linkedin,
+  Stethoscope,
+  Plane,
+  Sprout,
+  Tv,
+  Dumbbell,
+  Building2,
+} from "lucide-react";
 function App() {
   return (
     //with this className below, was created columns
@@ -12,13 +22,14 @@ function App() {
       {/*  */}
       {/*  */}
       <div className="mt-20 ml-5">
-        {/* <FacebookButton /> */}
         <div className="grid rounded-full h-20 w-20 border-2 border-blue-500 items-center justify-center mb-5">
           <Icon icon={Facebook} color="white" size={35} />
         </div>
+        {/*  */}
         <div className="grid rounded-full h-20 w-20 border-2  border-green-500 items-center justify-center mb-5">
           <Icon icon={Github} color="white" size={35} />
         </div>
+        {/*  */}
         <div className="grid rounded-full h-20 w-20 border-2 border-blue-300 items-center justify-center mb-5">
           <Icon icon={Linkedin} color="white" size={35} />
         </div>
@@ -31,16 +42,37 @@ function App() {
       </div>
       {/*  */}
       {/*  */}
-      <div className="mt-20 ml-5 ">
-        <h4 className="text-white font-k2d text-1xl mr-10 mb-1">Doctors</h4>
-        <div className="grid rounded-full h-20 w-20 border-2 border-blue-500 items-center justify-center mb-2">
+      <div className="mt-8 ml-5 ">
+        <div className="grid rounded-full h-20 w-20 border-2 border-red-500 items-center justify-center">
           <Icon icon={Stethoscope} color="white" size={28} />
         </div>
-        <h4 className="text-white font-k2d text-1xl mr-10 mb-1">Travel</h4>
-        <div className="grid rounded-full h-20 w-20 border-2 border-blue-500 items-center justify-center mb-2">
+        <h4 className="text-white font-k2d text-1xl mr-10 mb-2">Doctors</h4>
+        {/*  */}
+        <div className="grid rounded-full h-20 w-20 border-2 border-pink-300 items-center justify-center">
           <Icon icon={Plane} color="white" size={28} />
         </div>
-        <FacebookButton />
+        <h4 className="text-white font-k2d text-1xl mr-10 mb-2">Travel</h4>
+        {/*  */}
+        <div className="grid rounded-full h-20 w-20 border-2 border-green-800 items-center justify-center">
+          <Icon icon={Sprout} color="white" size={28} />
+        </div>
+        <h4 className="text-white font-k2d text-1xl mr-10 mb-2">Farming</h4>
+        {/*  */}
+        <div className="grid rounded-full h-20 w-20 border-2 border-purple-500 items-center justify-center">
+          <Icon icon={Tv} color="white" size={28} />
+        </div>
+        <h4 className="text-white font-k2d text-1xl mr-10 mb-2">Streaming</h4>
+        {/*  */}
+        <div className="grid rounded-full h-20 w-20 border-2 border-orange-500 items-center justify-center">
+          <Icon icon={Dumbbell} color="white" size={28} />
+        </div>
+        <h4 className="text-white font-k2d text-1xl mr-10 mb-2">GYM</h4>
+        {/*  */}
+        <div className="grid rounded-full h-20 w-20 border-2 border-yellow-500 items-center justify-center">
+          <Icon icon={Building2} color="white" size={28} />
+        </div>
+        <h4 className="text-white font-k2d text-1xl mr-10 mb-2">Business</h4>
+        {/*  */}
       </div>
       {/*  */}
       {/*  */}
--- src/components/ItemsKnowlege.tsx
+++ src/components/ItemsKnowlege.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement } from "react";
 function ItemKnowledge(): ReactElement {
   return (
     <>
-      <h2 className="font-k2d text-white text-[16px]">
+      <h2 className="font-k2d text-white text-[13px]">
         Node.Js, React, NestJS, Next.js, Angular, Fastify, CSS (Sass, Tailwind,
         Styled-components), SQL(Postgres & MySQL), NoSQL (MongoDB & Redis), TDD
         (Jest & Mocha), BullMQ, Docker, Express, React Native, Ionic, PHP
