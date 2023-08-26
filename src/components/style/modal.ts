56d66c518307dec5355ba1c0241a437ad59f1308
rafael-p-s <rafaelsky321@gmail.com>
2023-08-26 17:00:29 -0300

    Started modal

---
+++ src/components/style/modal.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+export const DivBrackgroundModal = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  background-color: #808080;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .div_modal_info {
+    width: 25vw;
+    height: 35vh;
+    background-color: white;
+    border-radius: 8px;
+    box-shadow: 1pt 1pt 5pt black;
+  }
+`;
